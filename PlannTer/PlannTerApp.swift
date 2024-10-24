1de7e5de8e9f8cac7e7d8e14916cdac8afd365f3
Jakub Czajkowski <and.jj.an@gmail.com>
2024-10-24 17:13:18 +0200

    Initial Commit

---
+++ PlannTer/PlannTerApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlannTerApp.swift
+//  PlannTer
+//
+//  Created by Jakub Czajkowski on 24/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct PlannTerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
