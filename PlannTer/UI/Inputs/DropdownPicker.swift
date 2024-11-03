7bf4caf8f8642eafbfa879285c21a8bc901724b8
Ania000 <annasidor00@gmail.com>
2024-11-03 16:32:59 +0100

    Create dropdown picker

---
+++ PlannTer/UI/Inputs/DropdownPicker.swift
@@ -0,0 +1,69 @@
+struct DropdownPicker: View {
+    let title: String
+    let options: [String]
+
+
+    @State private  var isExpanded = false
+    @State private var selectedOption = options[0]
+
+    var body: some View {
+        VStack(alignment: .leading) {
+            Text(title)
+                .font(.note)
+            
+            VStack {
+                HStack {
+                    Text(selectedOption)
+                    Spacer()
+                    Image(systemName: "chevron.right")
+                        .rotationEffect(.degrees(isExpanded ? 90 : 0))
+                }
+                .frame(height: 50)
+                .background(Color.white.opacity(0.3))
+                .padding(.horizontal, 30)
+                .overlay(
+                    Rectangle()
+                        .frame(height: 1)
+                        .foregroundColor(.primaryText),
+                    alignment: .bottom
+                )
+                .cornerRadius(10)
+                .font(.subtitle)
+                .onTapGesture (withAnimation: .snappy) {
+                    isExpande.toggle()
+                }
+            
+            
+                if isExpanded {
+                    VStack {
+                        ForEach(options, id: \.self) { option in
+                            HStack {
+                                Text(option)
+                                Spacer()
+                                if selectedOption == option {
+                                    Image(systemName: "checkmark")
+                                }
+                            }
+                            .frame(height: 50)
+                            .padding(.horizontal, 40)
+                            .font(.subtitle)
+                            .onTapGesture (withAnimation: .snappy) {
+                                selectedOption = option
+                                isExpanded.toggle()
+                            }
+                        }
+                    }
+                    .transition(.move(edge: .bottom))
+                }
+            }
+            .background(Color.white.opacity(0.3))
+            
+        }
+        .foregroundColor(.additionalText)
+    }
+}
+
+
+#Preview {
+    DropdownPicker(title: "Units", options: ["metric", "imperial"])
+}
