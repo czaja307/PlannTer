2e75f04c06a164f50771801196971e4232b922b9
Ania000 <annasidor00@gmail.com>
2024-11-03 13:53:25 +0100

    Added addRoom and Room tiles
    symbols are now displayed next to number of warnings on room tiles.
    add room tile with plus symbol now created

--- PlannTer/UI/Tiles/AddRoomTile.swift
+++ PlannTer/UI/Tiles/AddRoomTile.swift
@@ -2,9 +2,6 @@
 import SwiftUI
 
 struct RoomTile: View {
-    let roomName : String
-    let roomWarnings : Int
-    let numPlants : Int
     let listPosition : Int
     
     private let size : CGFloat = 2 * UIScreen.main.bounds.width/3
@@ -16,22 +13,19 @@ struct RoomTile: View {
         listPosition % 2 == 0 ? Color.primaryBackground : Color.secondaryBackground
     }
     
-    
     var body: some View {
         ZStack(alignment: .top){
             VStack(spacing: mainPadding) {
-                Text(roomName)
+                Text("Add room")
                     .padding(mainPadding)
                     .font(.mainText)
                     .multilineTextAlignment(.center)
                     .foregroundColor(.primaryText)
                 HStack(spacing: mainPadding) {
                     Spacer()
-                    InfoBubble(color: fgColor, value: numPlants, image: Image("PlantSymbol"), size: (size - 3 * mainPadding)/3)
+                    AddBubble(color: fgColor, size: (size - 3 * mainPadding)/3)
                     Spacer()
-                    InfoBubble(color: fgColor, value: roomWarnings, image: Image("WarnSymbol"), size: (size - 3 * mainPadding)/3)
-                    Spacer()
-                }
+                    }
                 .padding(mainPadding/2)
             }
             .padding(mainPadding)
@@ -40,41 +34,33 @@ struct RoomTile: View {
             .cornerRadius(15)
             .shadow(color: Color(.brown), radius: 5, x: 2, y: 4)
             HStack{
-                    Spacer()
-                    Image("PinImage")
-                        .resizable()
-                        .aspectRatio(contentMode: .fit)
-                        .frame(width: size/2, height: auto)
-                        .offset(x: 20, y: -size / 2)
-                    Spacer()
+                Spacer()
+                Image("PinImage")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: size/2, height: auto)
+                    .offset(x: 20, y: -size / 2)
+                Spacer()
             }
         }
     }
 }
 
 #Preview {
-    RoomTile(roomName: "osdfijldf", roomWarnings: 3, numPlants: 3)
+    RoomTile()
 }
 
 
-struct InfoBubble: View{
+struct AddBubble: View{
     let color : Color
-    let value : Int
-    let image : Image
     let size : CGFloat
     
     var body: some View {
-        VStack() {
-            HStack() {
-                image
-                    .resizable()
-                    .aspectRatio(contentMode: .fit)
-                    .frame(width: size/2, height: auto)
-                Text("\(value)")
-            }
-        }
-        .frame(width:size, height:size)
-        .background(color)
-        .cornerRadius(size/2)
+        Image("AddSymbol")
+            .resizable()
+            .aspectRatio(contentMode: .fit)
+            .frame(width:size, height:size)
+            .background(color)
+            .cornerRadius(size/2)
     }
 }
