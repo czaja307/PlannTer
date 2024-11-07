744e6565cb5fa9ef81114727136c9bd9a3a86fbe
Jakub Czajkowski <and.jj.an@gmail.com>
2024-11-07 17:35:42 +0100

    WIP

--- PlannTer/Modules/PlantModule/PlantDetailsView.swift
+++ PlannTer/Modules/PlantModule/PlantDetailsView.swift
@@ -1,19 +1,89 @@
 import SwiftUI
 
 struct PlantDetailsView: View {
-    //CZA: idk why it work like this but it builds
     @StateObject private var controller = PlantDetailsController(plant: PlantModel(id: UUID(), name: "Edytka"))
-
-
+    @State private var date = Date()
+    
+    
     var body: some View {
-         ZStack {
+        ZStack {
             Color(.primaryBackground)
                 .edgesIgnoringSafeArea(.all)
             VStack {
-                ScreenTitle(title: controller.plant.name)
-                    .padding(.top, 20)
+                // top bar
+                HStack {
+                    ScreenTitle(title: controller.plant.name)
+                    Image(systemName: "gearshape.fill")
+                        .font(.mainText)
+                        .foregroundColor(.primaryText)
+                        .padding(.trailing, 40)
+                }
+                // plant image and labels
+                HStack {
+                    ZStack {
+                        Rectangle()
+                            .fill(.white .opacity(0.4))
+                            .frame(width: 150, height: 150)
+                            .cornerRadius(15)
+                        Image(.edytka)
+                            .resizable()
+                            .aspectRatio(contentMode: .fill)
+                            .frame(width: 130, height: 130)
+                            .cornerRadius(15)
+                    }
+                    VStack {
+                        //TODO: refactor this
+                        Button(action: {}) {
+                            Text("Green room")
+                                .font(.note)
+                                .frame(width: 180, height: 30)
+                        }
+                        .buttonBorderShape(.capsule)
+                        .tint(.additionalBackground)
+                        .disabled(true)
+                        //------------
+                        Button(action: {}) {
+                            Text("Rose")
+                                .font(.note)
+                                .frame(width: 180, height: 30)
+                        }
+                        .buttonBorderShape(.capsule)
+                        .tint(.additionalBackground)
+                        //-------------
+                        Button(action: {}) {
+                            Text("Climbing")
+                                .font(.note)
+                                .frame(width: 180, height: 30)
+                        }
+                        .buttonBorderShape(.capsule)
+                        .tint(.additionalBackground)
+                    }
+                    .buttonStyle(.borderedProminent)
+                    .padding(.leading, 10)
+                }
+                .padding(.leading, 40)
+                .padding(.trailing, 40)
+                
+                //next - watering
+                HStack {
+                    DatePicker(
+                        "Next watering:",
+                        selection: $date,
+                        displayedComponents: [.date]
+                    )
+                    .datePickerStyle(.compact)
+                    .frame(width: 300)
+                    Text("32 days")
+                }
+                .font(.secondaryText)
+                .foregroundColor(.primaryText)
+                
                 Spacer()
-            } 
+            }
         }
     }
+}
+
+#Preview {
+    PlantDetailsView()
 }
