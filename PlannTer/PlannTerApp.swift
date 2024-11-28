b1467a9331f2e8062c1152dfabcdffef3fe7f12e
Jakub Czajkowski <and.jj.an@gmail.com>
2024-11-28 01:31:25 +0100

    feat: settings controller using SwiftData

--- PlannTer/PlannTerApp.swift
+++ PlannTer/PlannTerApp.swift
@@ -4,15 +4,30 @@
 //
 //  Created by Jakub Czajkowski on 24/10/2024.
 //
-
 import SwiftUI
+import SwiftData
 
 @main
 struct PlannTerApp: App {
     var body: some Scene {
         WindowGroup {
-            MainScreenView()
+            RootView()
         }
+        .modelContainer(for: [SettingsModel.self])
+    }
+}
+
+struct RootView: View {
+    @Environment(\.modelContext) private var context
+    @Query private var settings: [SettingsModel]
+    
+    var body: some View {
+        MainScreenView()
+            .onAppear(perform: {
+                if settings.isEmpty {
+                    context.insert(SettingsModel())
+                }
+            }).environment(settings.first ?? SettingsModel())
     }
 }
 
